4d1e690357f4aa3c1161d634b4583e31832006c2
kavin <khkavinha@gmail.com>
2018-05-03 15:36:29 -0700

    + Added 2 Layers for the main layout

--- app/src/main/java/com/example/kavin/wantedxtickets/MainActivity.java
+++ app/src/main/java/com/example/kavin/wantedxtickets/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.kavin.wantedxtickets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tool_bar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_create_account) {
+            Toast.makeText(MainActivity.this, "Create Account clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
